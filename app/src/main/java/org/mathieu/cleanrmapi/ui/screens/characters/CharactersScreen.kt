9d15ba1cef0f794b883ef15eeec4ed62dfd4b49b
Mathieu <mathieu.perroud@sporteasy.net>
2024-04-14 20:17:43 +0200

    [Feature/Characters] Updated the look&feel of main page

--- app/src/main/java/org/mathieu/cleanrmapi/ui/screens/characters/CharactersScreen.kt
+++ app/src/main/java/org/mathieu/cleanrmapi/ui/screens/characters/CharactersScreen.kt
@@ -5,28 +5,19 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,53 +25,47 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import coil.compose.SubcomposeAsyncImage
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.onEach
-import org.mathieu.cleanrmapi.domain.models.character.Character
-import org.mathieu.cleanrmapi.ui.core.Destination
+import org.mathieu.cleanrmapi.ui.core.composables.CharacterCard
 import org.mathieu.cleanrmapi.ui.core.composables.PreviewContent
-import org.mathieu.cleanrmapi.ui.core.navigate
-import org.mathieu.cleanrmapi.ui.core.theme.Purple40
+import org.mathieu.cleanrmapi.ui.core.composables.Screen
+import org.mathieu.cleanrmapi.ui.core.theme.OnPrimaryColor
+import org.mathieu.cleanrmapi.ui.core.theme.PrimaryColor
+import org.mathieu.projet2.R
 
 private typealias UIState = CharactersState
 private typealias UIAction = CharactersAction
 
 @Composable
 fun CharactersScreen(navController: NavController) {
-    val viewModel: CharactersViewModel = viewModel()
-    val state by viewModel.state.collectAsState()
 
-    LaunchedEffect(viewModel) {
-        viewModel.events
-            .onEach { event ->
-                if (event is Destination.CharacterDetails)
-                    navController.navigate(destination = event)
-            }.collect()
+    Screen(
+        viewModel = viewModel<CharactersViewModel>(),
+        navController = navController
+    ) { state, viewModel ->
+
+        Content(
+            state = state,
+            onAction = viewModel::handleAction
+        )
     }
-
-    CharactersContent(
-        state = state,
-        onAction = viewModel::handleAction
-    )
 
 }
 
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
-private fun CharactersContent(
+private fun Content(
     state: UIState = UIState(),
     onAction: (UIAction) -> Unit = { }
-) = Scaffold(topBar = {
+) = Scaffold(bottomBar = {
     Text(
         modifier = Modifier
-            .background(Purple40)
+            .background(PrimaryColor)
             .padding(16.dp)
             .fillMaxWidth(),
-        text = "Characters",
+        text = stringResource(R.string.characters),
         textAlign = TextAlign.Center,
-        color = Color.White,
+        color = OnPrimaryColor,
         fontSize = 16.sp,
         fontWeight = FontWeight.Medium
     )
@@ -90,7 +75,7 @@ private fun CharactersContent(
         modifier = Modifier
             .fillMaxSize()
             .padding(paddingValues),
-        contentAlignment = Alignment.Center
+        contentAlignment = Alignment.TopCenter
     ) {
         AnimatedContent(targetState = state.error != null, label = "") {
             state.error?.let { error ->
@@ -98,16 +83,20 @@ private fun CharactersContent(
                     modifier = Modifier.padding(16.dp),
                     text = error,
                     textAlign = TextAlign.Center,
-                    color = Purple40,
+                    color = PrimaryColor,
                     fontSize = 32.sp,
                     fontWeight = FontWeight.Medium,
                     lineHeight = 36.sp
                 )
-            } ?: LazyColumn {
+            } ?:
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2)
+            ) {
+
                 itemsIndexed(state.characters) { index, character ->
                     CharacterCard(
                         modifier = Modifier
-                            .padding(8.dp)
                             .clickable {
                                 onAction(CharactersAction.SelectedCharacter(character))
                             },
@@ -125,39 +114,9 @@ private fun CharactersContent(
 
 }
 
-@Composable
-private fun CharacterCard(
-    modifier: Modifier, character: Character
-) =
-    Row(
-        modifier = modifier
-            .shadow(5.dp)
-            .background(Color.White)
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 12.dp)
-        ,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-
-        SubcomposeAsyncImage(
-            modifier = Modifier
-                .size(44.dp)
-                .clip(CircleShape),
-            model = character.avatarUrl,
-            contentDescription = null
-        )
-
-        Spacer(modifier = Modifier.width(12.dp))
-
-        Text(text = character.name)
-
-    }
-
-
-
 @Preview
 @Composable
 private fun CharactersPreview() = PreviewContent {
-    CharactersContent()
+    Content()
 }
 
