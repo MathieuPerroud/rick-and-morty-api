8563d90a996f9698354f16f0882d82e03bbbda76
Mathieu <mathieu.perroud@sporteasy.net>
2024-04-14 20:17:59 +0200

    [UI/Core] Updated Theme with dark mode too

--- app/src/main/java/org/mathieu/cleanrmapi/ui/core/theme/Color.kt
+++ app/src/main/java/org/mathieu/cleanrmapi/ui/core/theme/Color.kt
@@ -1,11 +1,19 @@
 package org.mathieu.cleanrmapi.ui.core.theme
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BackgroundColor: Color
+    @Composable get() = MaterialTheme.colorScheme.background
+val OnBackgroundColor: Color
+    @Composable get() = MaterialTheme.colorScheme.onBackground
+val PrimaryColor: Color
+    @Composable get() = MaterialTheme.colorScheme.primary
+val OnPrimaryColor: Color
+    @Composable get() = MaterialTheme.colorScheme.onPrimary
+val SecondaryColor: Color
+    @Composable get() = MaterialTheme.colorScheme.secondary
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SurfaceColor: Color
+    @Composable get() = MaterialTheme.colorScheme.surface
--- app/src/main/java/org/mathieu/cleanrmapi/ui/core/theme/Theme.kt
+++ app/src/main/java/org/mathieu/cleanrmapi/ui/core/theme/Theme.kt
@@ -17,26 +17,21 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFFf0fbeb),
+    onPrimary = Color.DarkGray,
+    secondary = Color(0xFFe3f7fd),
+    background = Color(0xFF1f2d31),
+    surface = Color(0xFF223034),
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    background = Color.LightGray
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Color(0xFF6dd2ed),
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = Color(0xFFaae48b),
+    background = Color(0xFFFcFcFc),
+    surface = Color(0xFFFFFFFF),
+    onBackground = Color(0xFF3c545b),
+    error = Color(0xFFe89e89)
 )
 
 @Composable
@@ -47,15 +42,11 @@ fun LeTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
+
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
