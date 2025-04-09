fd304c08b1c7cb3a68fd5d0a7add6858edee0d7c
Corentin Piccioli <corentin.piccioli@diiage.org>
2025-04-09 15:25:12 +0200

    Forgotten comments

--- composeApp/src/nonJsMain/kotlin/org/mathieu/cleanrmapi/data/local/objects/LocationObject.kt
+++ composeApp/src/nonJsMain/kotlin/org/mathieu/cleanrmapi/data/local/objects/LocationObject.kt
@@ -5,6 +5,18 @@ import androidx.room.PrimaryKey
 import org.mathieu.cleanrmapi.data.local.RMDatabase
 import org.mathieu.cleanrmapi.data.validators.annotations.MustBeCommaSeparatedIds
 
+/**
+ *
+ * Représentation en base de données d'un lieu (Location) ne stockant que les informations essentielles
+ * provenant principalement de la source distante.
+ *
+ * @property id Identifiant unique du lieu.
+ * @property name Nom du lieu.
+ * @property type Type du lieu (ex: planète, vaisseau...).
+ * @property dimension Dimension dans laquelle se trouve le lieu.
+ * @property residentsIds Liste des IDs des résidents de ce lieu, séparés par des virgules.
+ * @property created Horodatage indiquant quand l'entité a été créée dans la base de données.
+ */
 @Entity(tableName = RMDatabase.LOCATION_TABLE)
 class LocationObject(
     @PrimaryKey
